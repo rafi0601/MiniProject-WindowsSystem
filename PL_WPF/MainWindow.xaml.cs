26babcd3b43807a2552a49f2b383a46886c46609
Rafi Goldis <rafig0601@gmail.com>
2019-02-13 20:05:54 +0200

    AAAAAAAAAAAAAAAAAAAAAAAAAALLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLL!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!

--- PL_WPF/MainWindow.xaml.cs
+++ PL_WPF/MainWindow.xaml.cs
@@ -23,7 +23,6 @@ namespace PL_WPF
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    [DebuggerDisplay("The main window")]
     public partial class MainWindow : Window
     {
         private BL.IBL bl = BL.Singleton.Instance;
@@ -37,19 +36,19 @@ namespace PL_WPF
             //new UI.TesterInterface.TesterRegisteraionWindow().Show();
             try
             {
-                new UI.AdminInterface.AdminWindow().Show();
-
-                bl.AddTester(new Tester("323947747", new Person.PersonName { FirstName = "Shmuel", LastName = "Garber" }, new DateTime(1950, 7, 13), Gender.male, "0547424870", new Address { Street = "יפו", HouseNumber = 5, City = "ירושלים" }, "1", 10, 30, Vehicle.tractor, new Schedule(new bool[,] { { true, true, true, true, true, true, true }, { true, true, true, true, true, true, true }, { true, true, true, true, true, true, true }, { true, true, true, true, true, true, true }, { true, true, true, true, true, true, true } }), 100));
-                bl.AddTester(new Tester("322680083", new Person.PersonName { FirstName = "Refael", LastName = "Goldis" }, new DateTime(1949, 5, 12), Gender.male, "0556824870", new Address { Street = "יפו", HouseNumber = 20, City = "ירושלים" }, "1", 6, 16, Vehicle.privateCar, new Schedule(new bool[,] { { false, false, true, true, true, true, true }, { true, true, true, true, true, true, true }, { true, true, true, true, true, true, true }, { true, true, true, true, true, true, true }, { true, true, true, true, true, true, true } }), 100));
-
-                Trainee trainee1 = new Trainee("212384507", new Person.PersonName { FirstName = "Yael", LastName = "katri" }, new DateTime(1995, 10, 6), Gender.female, "0541234567", new Address { Street = "יפו", HouseNumber = 2, City = "ירושלים" }, "1", Vehicle.privateCar, Gearbox.manual, "TheBest", new Person.PersonName { FirstName = "Shmuel", LastName = "Garber" }, 34, default);
-                Trainee trainee2 = new Trainee("323947739", new Person.PersonName { FirstName = "Asaf", LastName = "Levi" }, new DateTime(1948, 10, 6), Gender.female, "0541234567", new Address { Street = "יפו", HouseNumber = 17, City = "ירושלים" }, "1", Vehicle.privateCar, Gearbox.manual, "TheBest", new Person.PersonName { FirstName = "Shmuel", LastName = "Garber" }, 30, default);
-
-                bl.AddTrainee(trainee1);
-                bl.AddTrainee(trainee2);
+                //bl.AddTester(new Tester("323947747", new Person.PersonName { FirstName = "Shmuel", LastName = "Garber" }, new DateTime(1950, 7, 13), Gender.male, "0547424870", new Address { Street = "יפו", HouseNumber = 5, City = "ירושלים" }, "1", 10, 30, Vehicle.tractor, new Schedule(new bool[,] { { true, true, true, true, true, true, true }, { true, true, true, true, true, true, true }, { true, true, true, true, true, true, true }, { true, true, true, true, true, true, true }, { true, true, true, true, true, true, true } }), 100));
+                //bl.AddTester(new Tester("322680083", new Person.PersonName { FirstName = "Refael", LastName = "Goldis" }, new DateTime(1949, 5, 12), Gender.male, "0556824870", new Address { Street = "יפו", HouseNumber = 20, City = "ירושלים" }, "1", 6, 16, Vehicle.privateCar, new Schedule(new bool[,] { { false, false, true, true, true, true, true }, { true, true, true, true, true, true, true }, { true, true, true, true, true, true, true }, { true, true, true, true, true, true, true }, { true, true, true, true, true, true, true } }), 100));
+                //
+                //Trainee trainee1 = new Trainee("212384507", new Person.PersonName { FirstName = "Yael", LastName = "katri" }, new DateTime(1995, 10, 6), Gender.female, "0541234567", new Address { Street = "יפו", HouseNumber = 2, City = "ירושלים" }, "1", Vehicle.privateCar, Gearbox.manual, "TheBest", new Person.PersonName { FirstName = "Shmuel", LastName = "Garber" }, 34, default);
+                //Trainee trainee2 = new Trainee("323947739", new Person.PersonName { FirstName = "Asaf", LastName = "Levi" }, new DateTime(1948, 10, 6), Gender.female, "0541234567", new Address { Street = "יפו", HouseNumber = 17, City = "ירושלים" }, "1", Vehicle.privateCar, Gearbox.manual, "TheBest", new Person.PersonName { FirstName = "Shmuel", LastName = "Garber" }, 30, default);
+                //
+                //bl.AddTrainee(trainee1);
+                //bl.AddTrainee(trainee2);
 
                 //bl.AddTest(trainee1, new DateTime(2019, 2, 6, 10, 0, 0), trainee1.Address, trainee1.VehicleTypeTraining);
                 //bl.AddTest(trainee2, new DateTime(2019, 2, 6, 11, 0, 0), trainee2.Address, trainee2.VehicleTypeTraining);
+
+                new UI.AdminInterface.AdminWindow().Show();
 
                 new UI.TesterInterface.TesterWindow(bl.GetTester("323947747")).Show();
                 new UI.TesterInterface.TesterWindow(bl.GetTester("322680083")).Show();
